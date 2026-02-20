2fe1f3f36cf425deae2bd6715fd9161cdea57001
tariq <m.tariq.sarfraz.007@gmail.com>
2026-02-20 15:05:08 +0500

    inti push

---
+++ database/seeders/SuperUserSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class SuperUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $superAdmin = Role::create([
+            'name' => 'Super Admin'
+        ]);
+        $user = User::create([
+            'name' => 'Administrator',
+            'email' => 'tariq@movrs.com',
+            'password' => Hash::make(999999),
+            'is_active' => 1
+        ]);
+        $user->assignRole($superAdmin);
+
+        $user = User::create([
+            'name' => 'Rizwan',
+            'email' => 'rizwan@rose.com',
+            'password' => Hash::make(12345678),
+            'is_active' => 1
+        ]);
+        $user->assignRole($superAdmin);
+
+
+    }
+}
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="c-footer">
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.0</strong></div>
+</footer>
